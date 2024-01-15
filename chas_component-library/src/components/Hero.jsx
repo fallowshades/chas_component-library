7b53f2e2ca41d04b0ab3a4d4a4e37ef6e37c5e96
fallowShades <fallowshades@gmail.com>
2024-01-15 10:53:28 +0100

    button and alert in hero

--- chas_component-library/src/components/Hero.jsx
+++ chas_component-library/src/components/Hero.jsx
@@ -1,9 +1,23 @@
+import { Button } from 'flowbite-react'
+import { Alert } from 'flowbite-react'
+import { useState } from 'react'
 const Hero = () => {
+  const [toggle, setToggle] = useState(false)
+
+  const handleButtonClick = () => {
+    setToggle(!toggle)
+
+    // Set a timeout to close the alert after 3000 milliseconds (adjust the duration as needed)
+    setTimeout(() => {
+      setToggle(false)
+    }, 3000)
+  }
   return (
-    <div class="container">
-      <div class="section section1">
-        <div class="section1Text">
+    <div className="container">
+      <div className="section section1">
+        <div className="section1Text">
           <h2>Välkommen till vår kattsida</h2>
+          {toggle && <Alert color={'info'}>you are not welcome</Alert>}
           <p>
             Här kan du upptäcka spännande fakta om katter och deras beteende.
             Utforska våra artiklar och bilder och lär dig mer om dessa underbara
@@ -13,7 +27,7 @@ const Hero = () => {
             Om du älskar katter lika mycket som vi gör, varför inte gå med i vår
             kattklubb? Klicka på knappen nedan för att bli medlem!
           </p>
-          <button class="button">Bli medlem</button>
+          <Button onClick={handleButtonClick}>Bli medlem</Button>
         </div>
         <img src="images/1.webp" alt="Katt 1" />
       </div>
